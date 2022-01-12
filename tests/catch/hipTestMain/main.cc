32577fa5b3ce52319d8d47f3c5104e8a0da8730d
Jatin Chaudhary <ndjatin@gmail.com>
2022-01-12 15:20:46 -0800

    Bug fixes

--- tests/catch/hipTestMain/main.cc
+++ tests/catch/hipTestMain/main.cc
@@ -1,10 +1,11 @@
 #define CATCH_CONFIG_RUNNER
+#define CATCH_CONFIG_EXTERNAL_INTERFACES
 #include <hip_test_common.hh>
 #include <iostream>
 
-#if defined(HIP_TEST_REPORT_MODE)
+//#if defined(HIP_TEST_REPORT_MODE)
 
-class PartialReporter : public Catch::StreamingReporterBase {
+class PartialReporter : public Catch::StreamingReporterBase<PartialReporter> {
  public:
   using StreamingReporterBase::StreamingReporterBase;
 
@@ -12,21 +13,23 @@ class PartialReporter : public Catch::StreamingReporterBase {
     return "Reporter for testing TestCasePartialStarting/Ended events";
   }
 
-  void testCasePartialStarting(Catch::TestCaseInfo const& testInfo, uint64_t partNumber) override {
+  void testCasePartialStarting(Catch::TestCaseInfo const& testInfo, uint64_t partNumber) {
     std::cout << "TestCaseStartingPartial: " << testInfo.name << '#' << partNumber << '\n';
   }
 
   void testCasePartialEnded(Catch::TestCaseStats const& testCaseStats,
-                            uint64_t partNumber) override {
-    std::cout << "TestCasePartialEnded: " << testCaseStats.testInfo->name << '#' << partNumber
+                            uint64_t partNumber) {
+    std::cout << "TestCasePartialEnded: " << testCaseStats.testInfo.name << '#' << partNumber
               << '\n';
   }
+  virtual void assertionStarting( Catch::AssertionInfo const& assertionInfo ) { std::cout << assertionInfo.macroName << " - " << assertionInfo.capturedExpression << std::endl; }
+        virtual bool assertionEnded( Catch::AssertionStats const& assertionStats ) { return true; }
 };
 
 
 CATCH_REGISTER_REPORTER("partial", PartialReporter)
 
-#endif
+//#endif
 
 int main(int argc, char** argv) {
   auto& context = TestContext::get(argc, argv);
