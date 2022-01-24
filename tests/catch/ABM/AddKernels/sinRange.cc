c68e25655210ec35f8387314bcacded52e077bb8
Jatin Chaudhary <jatin.chaudhary@codeplay.com>
2022-01-24 13:34:56 +0000

    Make the range go from .01 to 0.5

--- tests/catch/ABM/AddKernels/sinRange.cc
+++ tests/catch/ABM/AddKernels/sinRange.cc
@@ -9,13 +9,13 @@ TEST_CASE("ABM_sin_range", "") {
 
   auto lambda = [] __device__ (float* a) { *a = asinf(*a); };
 
-  for (int i = 100; i <= 300; i++) {
+  for (int i = 100; i <= 300; i+=5) {
     float val = i / 100.;
     float res = 0.0f;
     HIP_CHECK(hipMemcpy(dval, &val, sizeof(float), hipMemcpyHostToDevice));
     kernel<<<1, 1>>>(dval, lambda);
     HIP_CHECK(hipMemcpy(&res, dval, sizeof(float), hipMemcpyDeviceToHost));
-    HIP_LOG_RESULT(val, std::to_string(res) + "SinResult")
+    HIP_LOG_RESULT(res, std::to_string(val) + "-SinResult")
   }
   HIP_CHECK(hipFree(dval));
 }
@@ -27,13 +27,13 @@ TEST_CASE("ABM_cos_range", "") {
 
   auto lambda = [] __device__ (float* a) { *a = acosf(*a); };
 
-  for (int i = 100; i <= 300; i++) {
+  for (int i = 100; i <= 300; i+=5) {
     float val = i / 100.;
     float res = 0.0f;
     HIP_CHECK(hipMemcpy(dval, &val, sizeof(float), hipMemcpyHostToDevice));
     kernel<<<1, 1>>>(dval, lambda);
     HIP_CHECK(hipMemcpy(&res, dval, sizeof(float), hipMemcpyDeviceToHost));
-    HIP_LOG_RESULT(val, std::to_string(res) + "CosResult")
+    HIP_LOG_RESULT(res, std::to_string(val) + "-CosResult")
   }
   HIP_CHECK(hipFree(dval));
 }
